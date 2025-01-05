1df97642d296c6e2fad84e49fe78e9dde9df8463
jay bodra <jaybodra01@gmail.com>
2025-01-05 18:06:33 +0530

    backend side user api created

---
+++ Backend/models/user.model.js
@@ -0,0 +1,32 @@
+// models/User.js
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true, // Automatically creates `createdAt` and `updatedAt` fields
+  }
+);
+
+export const User = mongoose.model('User', userSchema);
